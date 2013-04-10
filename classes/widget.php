86a683a3fbb1cf7c70ed7714de19cc3b954c5bf6
Alastair McGowan-Douglas <alastair@propcom.co.uk>
2013-04-10 11:42:44 +0100

    Docblocks

--- classes/widget.php
+++ classes/widget.php
@@ -8,6 +8,15 @@ class Widget extends \ViewModel
 
 	protected $_data = [];
 
+	/**
+	 * Retrieve data filed under $key, or $default if no data.
+	 *
+	 * This is the raw data before filtering.
+	 *
+	 * @param 	$key	string	Key to retrieve.
+	 * @param	$default	string	Optional. Return value if $key not found.
+	 * @return	mixed	Data set under $key or $default
+	 */
 	public function & get($key = null, $default = null)
 	{
 		if (isset($this->_data[$key]))
@@ -17,6 +26,14 @@ class Widget extends \ViewModel
 		return $default;
 	}
 
+	/**
+	 * Set data under $key to $value.
+	 *
+	 * @param	$key	string	Key to set
+	 * @param	$value	mixed	Value to set
+	 * @param	$filter	bool	Optional. Define whether to filter this value when it is later set on the view.
+	 * @return	$this
+	 */
 	public function set($key, $value, $filter = null)
 	{
 		is_null($filter) and $filter = $this->_auto_filter;
@@ -26,6 +43,11 @@ class Widget extends \ViewModel
 		return $this;
 	}
 
+	/**
+	 * Lazily sets the data into the real View object.
+	 *
+	 * Also applies the remembered $filter settings from set().
+	 */
 	public function after()
 	{
 		foreach ($this->_data as $key => $value)
@@ -34,6 +56,13 @@ class Widget extends \ViewModel
 		}
 	}
 
+	/**
+	 * Create a new Widget that uses widgets/$type as its view.
+	 *
+	 * The intention is that you subclass Widget and pass its type to this constructor.
+	 *
+	 * @param 	$type	string	The type of widget.
+	*/
     public function __construct($type)
     {
         $this->_type = $type;
@@ -43,21 +72,51 @@ class Widget extends \ViewModel
         $this->set('widget_type', $this->_type);
     }
 
+	/**
+	 * Deal with any GET string data from the request.
+	 *
+	 * @return	bool	Whether handling was performed.
+	 */
 	public function handle_get($data)
 	{
 		return true;
 	}
+
+	/**
+	 * Deal with any POST data from the request.
+	 *
+	 * When overriding, remember to return true iff you *handled* the POST data, even if there were errors.
+	 *
+	 * Only return false if you didn't want to use the data.
+	 *
+	 * @param	$data	array	POST data associative array.
+	 * @return	bool	Whether the data were handled by this widget.
+	 */
 
     public function handle_post($data)
     {
 		return true;
 	}
 
+	/**
+	 * Return any errors from the POST request.
+	 *
+	 * This is only called if handle_post returned true.
+	 *
+	 * @return	mixed	At minimum a value that boolifies to indicate whether there were POST errors
+	 */
     public function errors()
     {
         return false;
     }
 
+	/**
+	 * Return a string identifying the widget's type
+	 *
+	 * This is used to compare the POST value 'widget', to determine that this widget may have POST data for it.
+	 *
+	 * @return	string	Widget type
+	 */
     public function type()
     {
         return $this->_type;
